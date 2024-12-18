5307cd3fbec946764301a43fc501a87edaf9b0b0
thanh1110017d4 <thanhsktbox@gmail.com>
2024-12-18 12:16:16 +0700

    add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import Image from 'next/image';
 
 const Page = () => {
+  // Trạng thái của các API
   const [helloMessage, setHelloMessage] = useState<string>("");
   const [userInfo, setUserInfo] = useState<any>(null);
   const [products, setProducts] = useState<any[]>([]);
   const [sumResult, setSumResult] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
 
+  // Trao đổi thông tin thông qua API và trả về kết quả
   const fetchHelloMessage = async () => {
     const response = await fetch("http://localhost:3000/api/hello");
     const message = await response.text();
@@ -38,14 +41,14 @@ const Page = () => {
     const blob = await response.blob();
     setImageUrl(URL.createObjectURL(blob));
   };
-
+  // Giao diện
   return (
     <div className="container mx-auto p-4 bg-gray-100 text-gray-800">
       <header className="mb-8">
         <h1 className="text-4xl font-bold text-center">API Demo</h1>
       </header>
       <main>
-        {/* Welcome Message */}
+        <p>Hello</p>
         <section className="mb-8">
           <button
             onClick={fetchHelloMessage}
@@ -56,7 +59,7 @@ const Page = () => {
           <p className="mt-4">{helloMessage}</p>
         </section>
 
-        {/* User Information */}
+        <p>Get User Information</p>
         <section className="mb-8">
           <input
             id="userId"
@@ -94,7 +97,7 @@ const Page = () => {
           )}
         </section>
 
-        {/* Products */}
+        <p>Product Infomation</p>
         <section className="mb-8">
           <button
             onClick={fetchProducts}
@@ -111,7 +114,7 @@ const Page = () => {
           </ul>
         </section>
 
-        {/* Sum of Two Numbers */}
+        <p>Calc sum</p>
         <section className="mb-8">
           <input
             id="numA"
@@ -139,7 +142,7 @@ const Page = () => {
           <p className="mt-4">{sumResult}</p>
         </section>
 
-        {/* Image */}
+        <p>Get Image</p>
         <section className="mb-8">
           <button
             onClick={fetchImage}
@@ -150,6 +153,11 @@ const Page = () => {
           {imageUrl && <img src={imageUrl} alt="Fetched Image" className="mt-4" />}
         </section>
       </main>
+      <div style={{ display: 'list-item'}}>
+      <Image src="/1.jpg" alt="Image 1" width={1000} height={800}  />
+      <Image src="/hust.jpg" alt="Image 2" width={200} height={150} />
+      <Image src="/3.jpg" alt="Image 3"  width={1000} height={800} />
+    </div>
     </div>
   );
 };
